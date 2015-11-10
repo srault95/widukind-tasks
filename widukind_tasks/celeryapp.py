19f53a000cc60747e76503ec280e076028b12de7
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-11-10 15:31:16 +0100

    add mongolock and change environ namespaces

--- widukind_tasks/celeryapp.py
+++ widukind_tasks/celeryapp.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 
+from hashlib import md5
+
 from decouple import config as env_config
 
 from celery import Celery
 from celery.utils.log import get_task_logger
 
+from widukind_tasks.mongolock import MongoLock, MongoLockLocked
+from widukind_tasks import utils
+
 logger = get_task_logger(__name__)
 
-CELERY_MODE = env_config('CELERY_MODE', 'dev')
+CELERY_MODE = env_config('WIDUKIND_CELERY_MODE', 'prod')
 
 #'mongodb://mongodb/widukind?replicaSet=widukind'
 #CELERY_BROKER = env_config('CELERY_BROKER', 'mongodb://localhost/widukind')
-CELERY_BROKER = env_config('CELERY_BROKER', 'redis://localhost:6379/0')
+CELERY_BROKER = env_config('WIDUKIND_CELERY_BROKER', 'redis://localhost:6379/0')
 
 #print("__main__ : ", __name__)#widukind_tasks.celeryapp
 app = Celery()
 
-#broker=CELERY_BROKER
+task_locker = None
 
 class Config:
     BROKER_URL = CELERY_BROKER
@@ -79,29 +84,43 @@ ADMINS = []
 SERVER_EMAIL
 """
 
+#TODO: lock redis: https://pypi.python.org/pypi/python-redis-lock/2.3.0
+#TODO: default_retry_delay and max_retries depends of fetcher settings ?
 @app.task(#bind=True, 
           name="fetcher.run", ignore_result=True, default_retry_delay=30, max_retries=3)
 def fetcher_run_task(fetcher_name=None, dataset_code=None):    
     from widukind_tasks.tasks.fetchers import run_fetcher
+
     try:
-        return run_fetcher(fetcher_name=fetcher_name, dataset_code=dataset_code)
+        #expire 10 minutes - timeout = 5mn
+        #TODO: voir effet du timeout
+        key = md5("%s.%s" % (fetcher_name, dataset_code)).hexdigest()
+        with task_locker(key, 'fetcher.run', expire=600, timeout=300):
+            return run_fetcher(fetcher_name=fetcher_name, dataset_code=dataset_code)
+        
+    except MongoLockLocked as err:
+        logger.warning(str(err))
+        #TODO: retry progressive
+        fetcher_run_task.retry(kwargs=dict(fetcher_name=fetcher_name, dataset_code=dataset_code), exc=err)
+    
     except Exception as err:
         logger.error(err)
+        #TODO: retry sur quelles erreurs ?
         fetcher_run_task.retry(kwargs=dict(fetcher_name=fetcher_name, dataset_code=dataset_code), exc=err)
-        #avec ou sans raise, avec ou sans kwargs, NOK
-        #raise self.retry(exc=err)
 
 def main():
+    global task_locker
     try:
         #app.worker_main(argv=None)
+        mongo_client = utils.get_mongo_client()
+        db = client.get_default_database()
+        task_locker = MongoLock(client=client, db=db.name)
         app.start(argv=None)
     except KeyboardInterrupt:
         pass
     
 if __name__ == "__main__":
     """
-    set PYTHONPATH=..\dlstats;..\pysdmx
-    
     python -m widukind_tasks.celeryapp worker --loglevel=info
     
     > ok
